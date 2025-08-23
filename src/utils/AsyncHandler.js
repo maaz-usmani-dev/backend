7e558e259c9f60bdcc1a913d633ed9662114336a
maaz-usmani-dev <maaziftikhar1510@gmail.com>
2025-08-23 19:46:15 +0500

    add register controller

--- src/utils/AsyncHandler.js
+++ src/utils/AsyncHandler.js
@@ -1,25 +1,25 @@
 // Using Async/Await
     
-// const asyncHandler = (func) => async (req, res, next) => {
-//     try {
-//         await func(req, res, next)
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success:false,
-//             message: error.message
-//         })
-//     }
-// }
-// export { asyncHandler }
+const asyncHandler = (func) => async (req, res, next) => {
+    try {
+        await func(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message: error.message
+        })
+    }
+}
+export { asyncHandler }
 
 
 // // Using Promises
 
-const asyncHandler = (func) => {
-   return (req, res, next) => {
-        Promise.resolve(func(req, res, next))
-        .catch((err) => next(err))
-    }
-}
+// const asyncHandler = (func) => {
+//    return (req, res, next) => {
+//         Promise.resolve(func(req, res, next))
+//         .catch((err) => next(err))
+//     }
+// }
 
-export {asyncHandler}
+// export {asyncHandler}
