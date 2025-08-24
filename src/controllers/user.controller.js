1e59a7f484c6a1fcf7c00140c9d13c7e10c86ba0
maaz-usmani-dev <maaziftikhar1510@gmail.com>
2025-08-24 23:59:24 +0500

    create user login and user logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,27 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+
+const generateTokens = async (userId) => {
+    try {
+        const user = User.findById(userId)
+
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+    } catch (error) {
+        throw new ApiError(500, "Failed to generate Tokens")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
 
     // get userdata
+    if (!req.body) throw new ApiError(400, "Must Enter Data")
 
     const { username, email, fullname, password } = req.body
 
@@ -55,16 +73,88 @@ const registerUser = asyncHandler(async (req, res) => {
         "-password -refreshToken"
     )
     if (!createdUser) {
-        throw new ApiError(500,"Something went wrong while registration"); 
+        throw new ApiError(500, "Something went wrong while registration");
     }
 
     // return res
     return res.status(201).json(
-        new ApiResponse(200,createdUser,"User Created Successfully")
-        
+        new ApiResponse(200, createdUser, "User Created Successfully")
+
     )
 
 
 })
+const loginUser = asyncHandler(async (req, res) => {
 
-export { registerUser }
+    // get data
+    const { email, username, password } = req.body
+
+    // validate username or email
+    if (!(username || email))
+        throw new ApiError(500, "Username or email is required")
+
+    // find user
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+    if (!user)
+        throw new ApiError(404, "User Does not exist")
+    // check password
+    const passwordCorrect = await user.isPasswordCorrect(password)
+    if (!passwordCorrect) {
+        throw new ApiError(400, "Password is incorrect")
+    }
+    // generate access and refresh token
+    const { accessToken, refreshToken } = await generateTokens(user._id)
+
+    // either one more db query or update the user
+    user.refreshToken = refreshToken
+    const loggedInUser = user.select("-password -refreshToken")
+    // send cookies
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser,
+                accessToken,
+                refreshToken
+            },
+            "User Logged in successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async (req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(
+        new ApiResponse(200,{},"User Logged Out Successfully")
+    )
+})
+export { registerUser, loginUser, logoutUser }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 
-
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         cloudinary.config({
